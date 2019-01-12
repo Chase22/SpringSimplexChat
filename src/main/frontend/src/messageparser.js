7740543e74162190a66597be7757515346616c2a
chase <chase@mailbox.org>
2019-01-12 22:27:17 +0100

    Use moment.js in message parser and switch to div

--- src/main/frontend/src/messageparser.js
+++ src/main/frontend/src/messageparser.js
@@ -1,23 +1,25 @@
+import moment from 'moment';
+
 export function createChatMessage(value) {
-    const time = new Date(value.timestamp);
+    const time = moment(value.timestamp);
 
     let divMessage = document.createElement("div");
     divMessage.id = value.id;
     divMessage.classList.add("chat-message");
 
-    let spanTimestamp = createSpan(time.toLocaleTimeString(), "chat-message-timestamp");
-    let spanUser = createSpan(value.userId, "chat-message-user");
-    let spanMessage = createSpan(value.message, "chat-message-message");
+    let timestamp = createDiv(time.format('LTS'), "chat-message-timestamp");
+    let user = createDiv(value.userId, "chat-message-user");
+    let message = createDiv(value.message, "chat-message-message");
 
-    divMessage.appendChild(spanTimestamp);
-    divMessage.appendChild(spanUser);
-    divMessage.appendChild(spanMessage);
+    divMessage.appendChild(timestamp);
+    divMessage.appendChild(message);
+    divMessage.appendChild(user);
 
     return divMessage;
 }
 
-export function createSpan(innerText, classname) {
-    let span = document.createElement("span");
+export function createDiv(innerText, classname) {
+    let span = document.createElement("div");
     span.innerText = innerText;
     span.classList.add(classname);
     return span;
