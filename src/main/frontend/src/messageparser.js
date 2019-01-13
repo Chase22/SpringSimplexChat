4e9854e0da74b901f6ab7149a6ff5643c205136b
chase <chase@mailbox.org>
2019-01-13 12:00:48 +0100

    Changed styling

--- src/main/frontend/src/messageparser.js
+++ src/main/frontend/src/messageparser.js
@@ -4,23 +4,29 @@ export function createChatMessage(value) {
     const time = moment(value.timestamp);
 
     let divMessage = document.createElement("div");
+    let timestamp = createDiv(time.format('LTS'), "chat-message-timestamp");
+    let message = document.createElement("div");
+    let messageContent = createDiv(value.message, "chat-message-message-content");
+    let user = createDiv(value.userId, "chat-message-user");
+
     divMessage.id = value.id;
     divMessage.classList.add("chat-message");
-
-    let timestamp = createDiv(time.format('LTS'), "chat-message-timestamp");
-    let user = createDiv(value.userId, "chat-message-user");
-    let message = createDiv(value.message, "chat-message-message");
-
     divMessage.appendChild(timestamp);
-    divMessage.appendChild(message);
-    divMessage.appendChild(user);
+
+    message.classList.add("chat-message-message");
+
+
+    message.appendChild(messageContent);
+    message.appendChild(user);
+
+    divMessage.append(message);
 
     return divMessage;
 }
 
 export function createDiv(innerText, classname) {
     let span = document.createElement("div");
-    span.innerText = innerText;
+    span.innerHTML = innerText;
     span.classList.add(classname);
     return span;
 }
